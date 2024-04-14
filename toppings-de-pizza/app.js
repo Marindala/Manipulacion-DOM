3d72c813fef4f5d88422a3e5622ccfe5d706f281
Marina <95050756+Marindala@users.noreply.github.com>
2024-04-13 23:38:41 -0300

    study english now

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -1,6 +1,6 @@
 const contenedor = document.getElementById('contenedor');
 //console.log(contenedor);/* en coonsola en el servidor hago click en el div y observo todas las propiedades */
-console.log(typeof contenedor.innerHTML)
+console.log(typeof contenedor.innerHTML)/* inner interno */
 
 const titulo = document.getElementById('titulo');
-console.log(typeof titulo)
+console.log(typeof titulo) /* tagName tipo de etiqueta */
