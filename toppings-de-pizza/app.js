c4ad78b023e0c510ab092c3bf105ea4147496e3c
Marina <95050756+Marindala@users.noreply.github.com>
2024-04-13 23:35:47 -0300

    return typeof object

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -1,2 +1,6 @@
 const contenedor = document.getElementById('contenedor');
-console.log(contenedor);/* en coonsola en el servidor hago click en el div y observo todas las propiedades */
+//console.log(contenedor);/* en coonsola en el servidor hago click en el div y observo todas las propiedades */
+console.log(typeof contenedor.innerHTML)
+
+const titulo = document.getElementById('titulo');
+console.log(typeof titulo)
