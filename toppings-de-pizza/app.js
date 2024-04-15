5cc8ae017534ecda873dba8df5c782b0d11ac546
Marina <95050756+Marindala@users.noreply.github.com>
2024-04-14 21:22:07 -0300

    getElementsByClassName

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -4,3 +4,15 @@ console.log(typeof contenedor.innerHTML)/* inner interno */
 
 const titulo = document.getElementById('titulo');
 console.log(typeof titulo) /* tagName tipo de etiqueta */
+
+//class
+const toppings = document.getElementsByClassName('topping');
+console.log(toppings[0].id);
+/* {HTMLCollection(4)[
+    "0": {},
+    "1": {},
+    "2": {},
+    "3": {}
+]
+}
+ */
