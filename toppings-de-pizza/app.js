b04f049df267c4c364ed307667801702d7c63b97
Marina <marinalopezka@gmail.com>
2024-07-08 00:43:10 -0300

    elemnt and child

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -113,6 +113,7 @@ nuevoTopping.innerText = 'Nueva Clase';//se agrega a la lista un nuevo topping
 console.log(nuevoTopping.parentElement.parentElement)//secuencias//obtenemos el Padre del Padre
 //de la lista de topping//obtenemos body
 //console.log(nuevaLista.children) //accedo a los hijos
+//console.log(nuevaLista.firstElementChild)//obtener elementos HTML verificar con typeof
 
 
 
